e2c635aa5ba0a0eb25482836114f7d63e749af9b
BroJati <139361885+MaBroJati@users.noreply.github.com>
2025-09-20 17:36:05 +0700

    script.js

--- script.js
+++ script.js
@@ -10,3 +10,41 @@ prev.addEventListener('click', function(){
     let items = document.querySelectorAll('.item')
     document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
 })
+
+// Add touch support for mobile devices
+let slide = document.querySelector('.slide')
+let startX = 0
+let endX = 0
+
+slide.addEventListener('touchstart', function(e) {
+    startX = e.touches[0].clientX
+})
+
+slide.addEventListener('touchend', function(e) {
+    endX = e.changedTouches[0].clientX
+    handleSwipe()
+})
+
+function handleSwipe() {
+    let diffX = startX - endX
+
+    // Minimum swipe distance to trigger action
+    if (Math.abs(diffX) > 50) {
+        if (diffX > 0) {
+            // Swipe left - next item
+            next.click()
+        } else {
+            // Swipe right - previous item
+            prev.click()
+        }
+    }
+}
+
+// Add keyboard navigation support
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'ArrowLeft') {
+        prev.click()
+    } else if (e.key === 'ArrowRight') {
+        next.click()
+    }
+})
